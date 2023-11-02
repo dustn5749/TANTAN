153bf7f783c715112bd7bb727c2ad439d8a4e40b
kimmyoungsung <audtjd1201@naver.com>
2023-11-02 11:49:17 +0900

    동행 작업중

---
+++ src/main/java/com/team1/project/dto/UsDTO.java
@@ -0,0 +1,16 @@
+package com.team1.project.dto;
+import java.util.Date;
+
+public class UsDTO {
+    private int usNum;
+    private String cityName;
+    private String writer;
+    private int usCount;
+    private Date regDate;
+    private Date startDate;
+    private Date endDate;
+    private String title;
+    private String content; 
+
+}
+
