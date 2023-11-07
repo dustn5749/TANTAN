24cc2a4e3da9d265a322d125df63bfb20ddbbb1a
yeonsu <dustn5749@naver.com>
2023-11-07 13:06:46 +0900

    리뷰 정리 중

--- src/main/java/com/team1/project/controller/ReviewController.java
+++ src/main/java/com/team1/project/controller/ReviewController.java
@@ -1,23 +1,25 @@
-//package com.team1.project.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/review")
-//public class ReviewController {
-//
-//    // 리뷰 메인
-//    @RequestMapping(value = "/main")
-//    public String Main(reviewDTO board) throws Exception {
-////    	try {
-////			//model.addAttribute("result", boardService.boardPageList(board));
-////        } catch (Exception e) {
-////            e.printStackTrace();
-////        }
-//        return "reviewMain";
-//    }
-//    
-//    
-//    
-//}
+package com.team1.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.team1.project.dto.ReviewDTO;
+
+@Controller
+@RequestMapping("/review")
+public class ReviewController {
+
+    // 리뷰 메인
+    @RequestMapping(value = "/main")
+    public String Main(ReviewDTO board) throws Exception {
+//    	try {
+//			//model.addAttribute("result", boardService.boardPageList(board));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        return "reviewMain";
+    }
+    
+    
+    
+}
